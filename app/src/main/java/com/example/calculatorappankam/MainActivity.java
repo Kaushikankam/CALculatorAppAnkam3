2d507de602a867d1f96f5d3bb984336e04db0549
Kaushikankam <142595965+Kaushikankam@users.noreply.github.com>
2023-09-08 08:19:07 -0500

    complete only design and enchancement left

--- app/src/main/java/com/example/calculatorappankam/MainActivity.java
+++ app/src/main/java/com/example/calculatorappankam/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,47 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void clickMe(View v){
+    public void Add(View v){
+        EditText number1 =(EditText)findViewById(R.id.no1);
+        EditText number2 =(EditText)findViewById(R.id.no2);
+        EditText result =(EditText)findViewById(R.id.result);
 
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1+n2;
+        result.setText("  "+r);
+    }
+
+    public void minus(View v){
+        EditText number1 =(EditText)findViewById(R.id.no1);
+        EditText number2 =(EditText)findViewById(R.id.no2);
+        EditText result =(EditText)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1-n2;
+        result.setText("  "+r);
+    }
+
+    public void multiply(View v){
+        EditText number1 =(EditText)findViewById(R.id.no1);
+        EditText number2 =(EditText)findViewById(R.id.no2);
+        EditText result =(EditText)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1*n2;
+        result.setText("  "+r);
+    }
+
+    public void divide(View v){
+        EditText number1 =(EditText)findViewById(R.id.no1);
+        EditText number2 =(EditText)findViewById(R.id.no2);
+        EditText result =(EditText)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1/n2;
+        result.setText("  "+r);
     }
 }
