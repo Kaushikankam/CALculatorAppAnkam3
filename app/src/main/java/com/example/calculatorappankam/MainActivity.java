f171af7d1c941af55a4b02bf4ee4fab1d0c7d099
Kaushikankam <142595965+Kaushikankam@users.noreply.github.com>
2023-09-11 08:21:08 -0500

    final alost complete design done string left

--- app/src/main/java/com/example/calculatorappankam/MainActivity.java
+++ app/src/main/java/com/example/calculatorappankam/MainActivity.java
@@ -59,4 +59,53 @@ public class MainActivity extends AppCompatActivity {
         int r = n1/n2;
         result.setText("  "+r);
     }
+    public void square(View v) {
+        EditText number1 = (EditText) findViewById(R.id.no1);
+        EditText number2 = (EditText) findViewById(R.id.no2);
+        EditText result = (EditText) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1*n1;
+        result.setText("  " + r);
+    }
+    public void root(View v) {
+        EditText number1 = (EditText) findViewById(R.id.no1);
+        EditText number2 = (EditText) findViewById(R.id.no2);
+        EditText result = (EditText) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        double r = Math.sqrt(n1);
+
+
+
+        result.setText("  " + r);
+
+
+    }
+    public void clear(View v) {
+        EditText number1 = (EditText) findViewById(R.id.no1);
+        EditText number2 = (EditText) findViewById(R.id.no2);
+        EditText result = (EditText) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        int r = n1 * n1;
+        result.setText("  ");
+        number1.setText("  ");
+        number2.setText("   ");
+
+    }
+
+    public void power(View v) {
+        EditText number1 = (EditText) findViewById(R.id.no1);
+        EditText number2 = (EditText) findViewById(R.id.no2);
+        EditText result = (EditText) findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        double r = Math.pow(n1, n2);
+        result.setText("  " + r);
+    }
 }
